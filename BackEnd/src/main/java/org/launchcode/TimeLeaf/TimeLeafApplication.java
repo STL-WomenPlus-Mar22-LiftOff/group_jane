1d85c86b552cd813fdf1d13471944ebc11de07f2
Aoekae <dark_aoekae@yahoo.com>
2023-01-11 19:46:08 -0600

    doing rebase

---
+++ BackEnd/src/main/java/org/launchcode/TimeLeaf/TimeLeafApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.TimeLeaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeLeafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeLeafApplication.class, args);
+	}
+
+}
