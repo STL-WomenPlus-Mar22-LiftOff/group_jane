8ce345dfe47950391335c16a6607057dd8ab440e
Aoekae <dark_aoekae@yahoo.com>
2023-01-14 12:23:37 -0600

    I believe this should fix the ongoing issues with annotations

--- BackEnd/authentication/src/main/java/org/launchcode/TimeLeaf/data/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package main.java.org.launchcode.TimeLeaf.data;
-
-import main.java.org.launchcode.TimeLeaf.models.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-    User findByUsername(String username);
-
-}
