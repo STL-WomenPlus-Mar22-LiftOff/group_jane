e15129a561b78289dc6960d87ca34bceadcbd2a5
Aoekae <dark_aoekae@yahoo.com>
2023-01-20 09:37:33 -0600

    updated the controllers

--- src/main/java/com/gardenPlanner/gardenPlanner/controllers/EventCategoryController.java
+++ src/main/java/com/gardenPlanner/gardenPlanner/controllers/EventCategoryController.java
@@ -1,4 +1,51 @@
 package com.gardenPlanner.gardenPlanner.controllers;
 
+import com.gardenPlanner.gardenPlanner.data.EventCategoryRepository;
+import com.gardenPlanner.gardenPlanner.models.EventCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("eventCategories")
 public class EventCategoryController {
+
+    @Autowired
+    private EventCategoryRepository eventCategoryRepository;
+
+    @GetMapping
+    public String displayAllCategories(Model model) {
+        model.addAttribute("title", "All Categories");
+        model.addAttribute("categories", eventCategoryRepository.findAll());
+        return "eventCategories/index";
+    }
+
+    @GetMapping("create")
+    public String renderCreateEventCategoryForm(Model model) {
+        model.addAttribute("title", "Create Category");
+        model.addAttribute(new EventCategory());
+        return "eventCategories/create";
+    }
+
+    @PostMapping("create")
+    public String processCreateEventCategoryForm(@Valid @ModelAttribute EventCategory eventCategory,
+                                                 Errors errors, Model model) {
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Create Category");
+            model.addAttribute(new EventCategory());
+            return "eventCategories/create";
+        }
+
+        eventCategoryRepository.save(eventCategory);
+        return "redirect:";
+    }
+
 }
