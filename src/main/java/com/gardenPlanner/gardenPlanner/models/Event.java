15ba15546642a5f9a38156bfcafbea033dacbcf0
adrianadarris <adrianadarris@gmail.com>
2023-01-22 10:18:01 -0600

    cleaned up home controller and added links to index/home page

--- src/main/java/com/gardenPlanner/gardenPlanner/models/Event.java
+++ src/main/java/com/gardenPlanner/gardenPlanner/models/Event.java
@@ -1,7 +1,5 @@
 package com.gardenPlanner.gardenPlanner.models;
 
-import com.gardenPlanner.gardenPlanner.models.dto.AbstractEntity;
-
 import javax.persistence.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
--- src/main/java/com/gardenPlanner/gardenPlanner/models/User.java
+++ src/main/java/com/gardenPlanner/gardenPlanner/models/User.java
@@ -4,7 +4,6 @@ import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import com.gardenPlanner.gardenPlanner.models.dto.AbstractEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Entity
