084e46def2643e196d238dda7efb1ada213054bd
Aoekae <dark_aoekae@yahoo.com>
2023-01-22 19:14:53 -0600

    update to fix invalid mapping error

--- src/main/java/com/gardenPlanner/gardenPlanner/controllers/LoginController.java
+++ src/main/java/com/gardenPlanner/gardenPlanner/controllers/LoginController.java
@@ -2,23 +2,25 @@ package com.gardenPlanner.gardenPlanner.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("users")
 public class LoginController {
-    @GetMapping
+    @GetMapping("")
     public String loginWelcome(){
-        return "Login to view your garden";
+        return "users/login";
     }
-    @GetMapping("loginform")
-    public String loginForm(){
-        return "";
-    }
-    @GetMapping("loginconfirmed")
-    public String loginConfirmed(){
-        return "You are logged in!";
-    }
-    @GetMapping("logout")
-    public String logoutConfirmed(){
-        return "You have logged out";
-    }
+//    @GetMapping("")
+//    public String loginForm(){
+//        return "";
+//    }
+//    @GetMapping("loginconfirmed")
+//    public String loginConfirmed(){
+//        return "You are logged in!";
+//    }
+//    @GetMapping("logout")
+//    public String logoutConfirmed(){
+//        return "You have logged out";
+//    }
 }
