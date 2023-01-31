19bb0204f8ec695a76114822a53af40a9c5832ff
QueenYee <freedomside@yahoo.com>
2023-01-30 18:34:20 -0600

    frags & css update

--- src/main/java/com/gardenPlanner/gardenPlanner/controllers/HomeController.java
+++ src/main/java/com/gardenPlanner/gardenPlanner/controllers/HomeController.java
@@ -1,14 +1,20 @@
 package com.gardenPlanner.gardenPlanner.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class HomeController {
 
     @GetMapping(value = "")
     public String index() {
-        return "index";
+        return "home";
     }
 
 }
